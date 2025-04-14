df6448ae303f15d0a9f3e559e7eb5bdb0f50695d
Habtszz27 <habtszzcd1@gmail.com>
2025-04-14 12:39:16 +0300

    Add custom landing page and components

--- server/index.js
+++ server/index.js
@@ -1,1 +1,4 @@
 const express = require("express"); const app = express(); const feedbackRoute = require("./routes/feedback"); app.use("/api/feedback", feedbackRoute); app.listen(5000, () => console.log("Server running"));
+app.get('/api/feedback', (req, res) => {
+  res.json({ message: 'Backend is working fine!' });
+});
