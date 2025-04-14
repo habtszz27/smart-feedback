c30a953051f0cf912522f000c9378226e79606e8
Habtszz27 <habtszzcd1@gmail.com>
2025-04-14 12:43:10 +0300

    Add custom landing page and components

--- server/index.js
+++ server/index.js
@@ -1,4 +1,10 @@
-const express = require("express"); const app = express(); const feedbackRoute = require("./routes/feedback"); app.use("/api/feedback", feedbackRoute); app.listen(5000, () => console.log("Server running"));
+const express = require("express"); const app = express(); const feedbackRoute = require("./routes/feedback"); app.use("/api/feedback", feedbackRoute); app.listen(5000, () => console.log("Server running"))
+   const cors = require('cors');
+
+app.use(cors({
+  origin: 'https://smart-feedback-client.onrender.com', // change to your actual client URL
+  credentials: true
+})); ;
 app.get('/api/feedback', (req, res) => {
   res.json({ message: 'Backend is working fine!' });
 });
